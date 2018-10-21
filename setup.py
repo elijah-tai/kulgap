62777106917be4c14c301ad678138bfa178b5903
Elijah Tai <elijah.tai@outlook.com>
2018-10-21 13:41:29 -0400

    Up version to 0.1.0.dev0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='kulgap',
-    version='0.1.dev0',
+    version='0.1.0.dev0',
     packages=['kulgap', ],
     author='Elijah Tai, Janosch Ortmann',
     author_email='elijah.tai@outlook.com',
